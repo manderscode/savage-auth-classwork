a2075608e52e30498bc8a84c6f9bd9270968ba75
Resilient Coders <resilientcoders@Kerlins-MacBook-Pro.local>
2022-05-30 23:36:57 -0400

    SavageAuth Classwork

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = { //exporting an object
+
+    'url' : 'mongodb+srv://mawong2:eVUa3qBDYCkoNjyi@cluster0.3xblz.mongodb.net/savageauth?retryWrites=true&w=majority', 
+    'dbName': 'savageauth'
+};
